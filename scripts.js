46c7787b34f895867e203786eb90d82d8fd9c115
J. Alejandro H. Duin <alejandroduin@gmail.com>
2024-04-10 22:37:27 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+var nombre, email, phone;
+
+function myFunction() {
+  document.getElementById("regNombre").textContent = nombre;
+  document.getElementById("regEmail").textContent = email;
+  document.getElementById("regPhone").textContent = phone;
+}
+
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  nombre = document.getElementById("nombre").value;
+  email = document.getElementById("email").value;
+  phone = document.getElementById("phone").value;
+
+  console.log("Nombre: " + nombre);
+  console.log("Email: " + email);
+  console.log("Número de celular: " + phone);
+});
